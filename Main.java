a68f04756c6de1d2bf78d5d4ee3982aef82b8365
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-21 13:10:23 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        // Add keys and values (Country, City)
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+
+        System.out.println(capitalCities);
+
+        String Eng = capitalCities.get("England");
+
+        capitalCities.remove("England");
+
+        //capitalCities.clear();
+
+        int Size = capitalCities.size();
+
+        // Print keys
+        for (String i : capitalCities.keySet()) {
+            System.out.println(i);
+        }
+
+        // Print values
+        for (String i : capitalCities.values()) {
+            System.out.println(i);
+        }
+
+        // Print keys and values
+        for (String i : capitalCities.keySet()) {
+            System.out.println("key: " + i + " value: " + capitalCities.get(i));
+        }
+
+        HashMap<String, Integer> people = new HashMap<String, Integer>();
+
+        // Add keys and values (Name, Age)
+        people.put("John", 32);
+        people.put("Steve", 30);
+        people.put("Angie", 33);
+
+        for (String i : people.keySet()) {
+            System.out.println("key: " + i + " value: " + people.get(i));
+        }
+
+        int Age = people.get("John");
+
+        for (Map.Entry<String, Integer> pair : people.entrySet()) {
+            System.out.println(String.format(pair.getKey() + " " +  pair.getValue()));
+        }
+
+
+    }
+
+}
